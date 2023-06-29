549962e4462a32baca2f2a1756ceab78dbda0d93
kotovvv <kotovvv@gmail.com>
2023-06-29 22:30:55 +0300

    type display add url

--- components/widgets/src/js/TypeDisplay.js
+++ components/widgets/src/js/TypeDisplay.js
@@ -5,23 +5,24 @@ import { createPortal } from 'react-dom'
 const TypeDisplay = memo(({ inDisplay, switchInDisplay, container }) => {
 
   return createPortal(<div className="type-display">
-    <a href={new URL(window.location.origin + window.location.pathname + '?pl=bloks')}><input id="bloks" type="radio" name="typedisplay" value="bloks" checked={inDisplay === 'bloks'} onChange={switchInDisplay} />
-    </a>
-    <label className="bloks" htmlFor="bloks" ><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="9" height="9" fill="white"></rect>
-      <rect x="14" width="9" height="9" fill="white"></rect>
-      <rect y="14" width="9" height="9" fill="white"></rect>
-      <path d="M14 14H23V23H14V14Z" fill="white"></path>
-    </svg></label>
-    <input id="lines" type="radio" name="typedisplay" value="lines" checked={inDisplay === 'lines'} onChange={switchInDisplay} />
-    <label className="lines" htmlFor="lines" ><svg width="27" height="23" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="9" height="9" fill="white"></rect>
-      <rect x="18" y="14" width="9" height="9" fill="white"></rect>
-      <line x1="13" y1="1.5" x2="27" y2="1.5" stroke="white"></line>
-      <line x1="13" y1="5.5" x2="27" y2="5.5" stroke="white"></line>
-      <line y1="15.5" x2="14" y2="15.5" stroke="white"></line>
-      <line y1="20.5" x2="14" y2="20.5" stroke="white"></line>
-    </svg></label>
+    <a href={new URL(window.location.origin + window.location.pathname + '?pl=bloks')} onClick={(e) => { e.preventDefault }}><input id="bloks" type="radio" name="typedisplay" value="bloks" checked={inDisplay === 'bloks'} onChange={switchInDisplay} />
+
+      <label className="bloks" htmlFor="bloks" ><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="9" height="9" fill="white"></rect>
+        <rect x="14" width="9" height="9" fill="white"></rect>
+        <rect y="14" width="9" height="9" fill="white"></rect>
+        <path d="M14 14H23V23H14V14Z" fill="white"></path>
+      </svg></label></a>
+    <a href={new URL(window.location.origin + window.location.pathname)} onClick={(e) => { e.preventDefault }}>
+      <input id="lines" type="radio" name="typedisplay" value="lines" checked={inDisplay === 'lines'} onChange={switchInDisplay} />
+      <label className="lines" htmlFor="lines" ><svg width="27" height="23" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="9" height="9" fill="white"></rect>
+        <rect x="18" y="14" width="9" height="9" fill="white"></rect>
+        <line x1="13" y1="1.5" x2="27" y2="1.5" stroke="white"></line>
+        <line x1="13" y1="5.5" x2="27" y2="5.5" stroke="white"></line>
+        <line y1="15.5" x2="14" y2="15.5" stroke="white"></line>
+        <line y1="20.5" x2="14" y2="20.5" stroke="white"></line>
+      </svg></label></a>
   </div >,
     container
   )
